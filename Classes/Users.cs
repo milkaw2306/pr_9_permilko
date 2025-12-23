3f960a75ccf39365c602a32e03b81d6cbff32fbf
student-a502 <student-a502@STUD-RM21-A502>
2025-12-23 14:28:19 +0500

    Созданный классы для приложения.permilko

--- Classes/Users.cs
+++ Classes/Users.cs
@@ -1,20 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.Extensions.Logging;
-
-namespace pr_9_permilko.Classes
+﻿namespace pr_9_permilko.Classes
 {
     public class Users
     {
         public long IdUser { get; set; }
-        public List<Events> Events { get; set; }
+        public List<Events> Events {get; set; }
         public Users(long idUser)
         {
             IdUser = idUser;
-            Events = new List<Events>();
+            Events = new List<Events>();        
         }
     }
 }
